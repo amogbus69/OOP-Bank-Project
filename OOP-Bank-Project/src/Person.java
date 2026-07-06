3201ba8ee6d56c6761226e963f0958aa7cfbe339
amogbus69 <sgcaldera@miners.utep.edu>
2026-07-06 11:24:39 -0600

    Add files via upload

---
+++ OOP-Bank-Project/src/Person.java
@@ -0,0 +1,13 @@
+public abstract class Person {
+    private String userID;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String address;
+    private String phoneNumber;
+    private String username;
+    private String password;
+    private String userType;
+
+    
+}
