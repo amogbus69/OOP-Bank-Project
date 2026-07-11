eb31d089ae8f7992d7bf8f307549777c17489a72
amogbus69 <sgcaldera@miners.utep.edu>
2026-07-10 21:08:20 -0600

    Update BankManager.java

--- src/BankManager.java
+++ src/BankManager.java
@@ -1,6 +1,17 @@
 public class BankManager extends Person{
-    
-    public void managerMenu(){
-        
+
+    public BankManager(int id,
+                       String first,
+                       String last,
+                       String dob,
+                       String address,
+                       String phone,
+                       String username,
+                       String password){
+
+        super(id,first,last,dob,address,
+                phone,username,password,"Bank Manager");
+
     }
+
 }
