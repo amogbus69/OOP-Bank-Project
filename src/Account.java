c723df4eca0651342e73f862d12ce02aa8b38867
amogbus69 <sgcaldera@miners.utep.edu>
2026-07-10 21:06:35 -0600

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,28 @@
 public abstract class Account {
-    private String accountNumber;
-    private double accountBalance;
 
-    
+    protected int accountNumber;
+    protected double balance;
+
+    public Account(int accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+
+        if(amount > 0)
+            balance += amount;
+
+    }
+
+    public abstract boolean withdraw(double amount);
+
 }
